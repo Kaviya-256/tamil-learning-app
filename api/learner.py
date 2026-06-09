2b84187185c0b3b59690bb04c36c9cb910a5a2ce
Kaviya-256 <kaviyap.sivan25@gmail.com>
2026-06-09 16:27:59 +0530

    commit 28

--- api/learner.py
+++ api/learner.py
@@ -3,6 +3,7 @@ from bson import ObjectId
 
 from utils.role_auth import require_roles
 from database.mongo import profile_collection
+from schema import ThemeColorEnum
 
 router = APIRouter()
 
@@ -11,10 +12,17 @@ router = APIRouter()
 async def get_learner_profile(learner = Depends(require_roles(['learner'], [profile_collection]))):
 
     learner_id = learner.get('id')
-    data = await profile_collection.find_one(
-        {'_id': ObjectId(learner_id)},
-        {'username':1, 'name':1, 'age':1, 'grade':1, 'theme_color':1, '_id':0})
-    
+    data = await profile_collection.find_one({'_id': ObjectId(learner_id)})
+
     if not data:
         raise HTTPException(status_code=404, detail="Learner not found")
-    return data
+
+    learner = {
+        'username': data.get('username'),
+        'name': data.get('name'),
+        'age': data.get('age'),
+        'grade': data.get('grade'),
+        'theme_color': getattr(ThemeColorEnum.__members__.get(data.get('theme_color')), 'value', None)
+    }
+    
+    return learner
